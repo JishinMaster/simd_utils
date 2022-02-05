b73d79af460fbfca110149ca7aa9beb4b99ae692
JishinMaster <francois.turban@gmail.com>
2022-02-05 18:15:21 +0100

    minor corrections

--- simd_test.c
+++ simd_test.c
@@ -8746,7 +8746,7 @@ for (int i = 0; i < len; i++){
     l2_err(inout4, inout2_ref, len);
 #endif
 
-#ifdef AVX
+#ifdef AVX512
     clock_gettime(CLOCK_REALTIME, &start);
     cart2pol2D512f(inout, inout2, inout3, inout4, len);
     clock_gettime(CLOCK_REALTIME, &stop);
